06ffe452abafffe28d6397b0688f4c023c763880
Aditya Agrawal <aditya.agrawal0309@gmail.com>
2021-09-02 16:35:29 +0530

    style: signup page

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,3 +1,4 @@
+import "./Signup.css";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext/AuthContext";
@@ -22,38 +23,50 @@ export function Signup() {
   };
 
   return (
-    <>
-      <h1> Signup </h1>
-      <form onSubmit={(e) => signupHandler(e)}>
-        <label htmlFor="username"> Username </label>
-        <input
-          type="text"
-          placeholder="Username"
-          name="username"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <label htmlFor="email"> email </label>
-        <input
-          type="text"
-          placeholder="email"
-          name="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="password"> password </label>
-        <input
-          type="text"
-          placeholder="password"
-          name="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit"> signup </button>
-      </form>
-    </>
+    <div className="signup-page">
+      <div className="signup-page-content">
+        <div className="signup-form-heading">
+          <h1> Signup </h1>
+        </div>
+        <div className="signup-form">
+          <form onSubmit={(e) => signupHandler(e)}>
+            <label htmlFor="username" className="label"> Username </label>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              id="username"
+              value={username}
+              className="input-text input-text-lg input-text-form"
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <label htmlFor="email" className="label"> Email </label>
+            <input
+              type="email"
+              placeholder="email"
+              name="email"
+              id="email"
+              value={email}
+              className="input-text input-text-lg input-text-form"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <label htmlFor="password" className="label"> Password </label>
+            <input
+              type="text"
+              placeholder="password"
+              name="password"
+              id="password"
+              value={password}
+              className="input-text input-text-lg input-text-form"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <button type="submit" className="btn btn-primary btn-signup"> signup </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
